f607722899f38e29facde4c6e4e48203675d086e
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-23 23:14:05 +0530

    Harmonic number program

---
+++ HarmonicNo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public  class HarmonicNo
+    {
+        public static void FindHarmonic()
+        {
+            double n = Convert.ToInt32(Console.ReadLine());
+            double Harmonic = 1;
+
+            for (float i = 0; i < n; i++)
+            {
+                Harmonic = Harmonic + 1 / i;
+                Console.WriteLine(Harmonic);
+            }
+        }
+    }
+}
