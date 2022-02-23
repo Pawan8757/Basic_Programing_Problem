548aec39a1f7f2633b18724a1e6999a4bc389ba7
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-23 23:02:48 +0530

    Adding greeting Program

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BasicCoreProgram
+{
+
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Basic Core Programming");
+            //**Largest.FindingLargestNo();
+            //Remainder.FindingQuotientRemainder();
+            //SwappingNo.SwapTwoNo()
+            //VowelConsonent.CheckVowelConsonent();
+            //Prime.CheckPrime();
+            //EvenOrOdd.DeterminingEvenOrOdd();
+            //HarmonicNo.FindHarmonic();
+            //LeapYear.CheckingLeapYear();
+            //PowerOfTwo.CalculatingPowerOfATwo();
+            //CoinFlipPerc.FindingPercOfCoinFlip();
+        }
+    }
+}
