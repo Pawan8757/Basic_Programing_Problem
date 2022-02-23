a5af471fb6b5c45f505c5a602c7f58c2721b6631
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-23 23:15:39 +0530

    adding factor program

---
+++ Prime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class Prime
+    {
+        public static void CheckPrimeFactorial()
+            
+            {
+                int n = 6;
+                double fact = 1;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    fact = fact * i;
+                }
+                Console.WriteLine("factorial of {0}={1}", n, fact);
+                Console.ReadLine();
+            }
+        }
+    }
