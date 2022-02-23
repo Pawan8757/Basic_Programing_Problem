ef83c95b70bc0e6fdd78001b619f15d4d6284f5a
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-23 23:28:20 +0530

    checking Vowel or Consonent

---
+++ VowelConsonent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class VowelConsonent
+    {
+        public static void CheckVowelConsonent()
+        {
+            Console.WriteLine("Enter any Character");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine(ch + " is a vowel");
+            }
+            else if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+            {
+                Console.WriteLine(ch + " is a Constant");
+            }
+            else
+            {
+                Console.WriteLine("Enter the valid Character");
+            }
+        }
+
+        }
+
+
+    }
+
