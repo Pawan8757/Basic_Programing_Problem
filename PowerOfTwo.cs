1449367e2488783503cb937d8c2982c319bdfdce
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-23 23:12:32 +0530

    Power of Two

---
+++ PowerOfTwo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class PowerOfTwo
+    {
+        public static void CalculatingPowerOfATwo()
+        {
+            int n = 4, y = 2, x = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                x=x*y;
+
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
